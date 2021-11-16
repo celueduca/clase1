39b80f67db15a19528f7d2f146fdb2710edbdc52
Bernardo Zuluaga <bernardo.zuluaga@celuweb.com>
2021-11-16 11:06:00 -0500

    Solucion de conflictos

--- saludos/index.js
+++ saludos/index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-console.log("Hola soy Rodrigo Padilla Naranjo")
-=======
 console.log("Hola soy Alexander Zuluaga");
 console.log("Hola soy Nicolas Gonzalez");
->>>>>>> 67fcafaee6dd906f77a4d53dcbdea543ed08e85c
+console.log("Hola soy Rodrigo Padilla Naranjo");
